7fe2950b9a8e05cd1993ed0f661b342432923708
Denis Reinoso <reinoso.denis@gmail.com>
2020-07-08 23:22:39 -0300

    Add logic for purchase and done buttons
    Signed-off-by: Denis Reinoso <reinoso.denis@gmail.com>

--- presentation/src/main/kotlin/com/reactions/deathlines/presentation/ui/albumdetail/AlbumDetailFragment.kt
+++ presentation/src/main/kotlin/com/reactions/deathlines/presentation/ui/albumdetail/AlbumDetailFragment.kt
@@ -1,21 +1,48 @@
 package com.reactions.deathlines.presentation.ui.albumdetail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.reactions.deathlines.presentation.R
+import androidx.navigation.findNavController
+import com.reactions.deathlines.presentation.databinding.FragmentAlbumDetailBinding
 import com.reactions.deathlines.presentation.ui.base.BaseFragment
+
 
 class AlbumDetailFragment : BaseFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_album_detail, container, false)
+    private lateinit var binding: FragmentAlbumDetailBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlbumDetailBinding.inflate(inflater, container, false)
+        val bindingUnmutable = binding!!
+        return bindingUnmutable.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val currentTrackId = arguments?.get("trackId").toString()
+        binding.btnPurchase.setOnClickListener(::onPurchaseBtnClicked)
+        binding.btnDone.setOnClickListener(::onDoneBtnClicked)
         Log.d(tag, "onViewCreated currentTrackId: $currentTrackId")
     }
+
+    fun onDoneBtnClicked(view: View) {
+        view.findNavController().navigate(AlbumDetailFragmentDirections.actionAlbumDetailFragmentToHomeFragment())
+    }
+
+    fun onPurchaseBtnClicked(view: View) {
+        val iTunesPurchaseUrl = "https://www.apple.com/itunes/"
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(iTunesPurchaseUrl)
+        startActivity(intent)
+    }
+
+
 }
